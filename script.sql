269717e99710d0cee032ad24913e450f58210864
bt <felix.blanco@utec.edu.pe>
2020-06-19 00:38:09 -0500

    fix(Tables): Changing types

--- script.sql
+++ script.sql
@@ -6,66 +6,68 @@
 ---Renzo Tenazoa
 
 CREATE TABLE Users(
-    id BIGINT PRIMARY KEY,
-    imagen VARCHAR(255),
-    Username VARCHAR(255)
+    id BIGINT PRIMARY KEY,  -- VARCHAR cambiar en el overleaf
+    imagen VARCHAR(255),     
+    Username VARCHAR(50)
 );
 
 CREATE TABLE Record(
-    id BIGINT PRIMARY KEY,
-    player VARCHAR(255),
-    Catchs DATE,
-    time_record TIME
+    id BIGINT PRIMARY KEY, -- VARCHAR cambiar en el overleaf
+    player VARCHAR(50),
+    Catchs DATE,    -- cambiar en el overleaf
+    time_record INT    -- INT cambiar en el overleaf
 );
 
 CREATE TABLE Course(
-    id BIGINT PRIMARY KEY,
-    thumbnail VARCHAR(256),
-    creation DATE
+    id BIGINT PRIMARY KEY, -- VARCHAR
+    thumbnail VARCHAR(255), -- VARCHAR cambiar en el overleaf
+    creation DATE   -- eliminar del diccionario difficulty
 );
 
 CREATE TABLE Country(
-    cname VARCHAR(255),
-    flag VARCHAR(255),
-    PRIMARY KEY (cname)
+    cname VARCHAR(50) PRIMARY KEY,
+    flag VARCHAR(3),
 );
 
 CREATE TABLE Metadata(
-    Course_id BIGINT,
-    first_clear VARCHAR(255),
-    tag VARCHAR(255),
+    Course_id BIGINT,   -- VARCHAR cambiar en el overleaf
+    first_clear VARCHAR(50), -- foreign key, cambiar en el overleaf
+    tag VARCHAR(255), -- INT cambiar en el overleaf
     starts INT,
     player_tweets INT,
     clears INT,
     attemps INT,
     clear_rate FLOAT,
     FOREIGN KEY (Course_id) REFERENCES Course (id)
+    FOREIGN KEY (first_clear) REFERENCES Users (Username)
 );
 
 CREATE TABLE MarioVersion(
-    difficulty VARCHAR(255),
-    Course_id BIGINT,
+    difficulty VARCHAR(15),
+    Course_id BIGINT,   -- Añadir en el overleaf
     FOREIGN KEY (Course_id) REFERENCES Course(id) 
 );
 
 CREATE TABLE MarioBros(
-    game_style VARCHAR(255) PRIMARY KEY,
-    mversion VARCHAR(255),
-    difficulty VARCHAR(255)
+    game_style VARCHAR(20) PRIMARY KEY, -- Añadir en el oveleaf
+    mversion VARCHAR(30), -- Cambiar en el overleaf
+    difficulty VARCHAR(15)
 );
 
 
 CREATE TABLE MarioWorld(
-    game_style VARCHAR(255) PRIMARY KEY,
-    año INT,
-    difficulty VARCHAR(255)
+    game_style VARCHAR(20) PRIMARY KEY, -- Añadir en el oveleaf
+    año INT,    -- Cambiar en el oveleaf
+    difficulty VARCHAR(15) 
 );
 
 CREATE TABLE MarioBrosU(
-    game_style VARCHAR(255) PRIMARY KEY,
+    game_style VARCHAR(20) PRIMARY KEY,
     isBeta BOOLEAN,
-    difficulty VARCHAR(255)
+    difficulty VARCHAR(15)
 );
+
+-- Cambiarlo en overlaf, desde AQUÍ
 
 CREATE TABLE Uses (
     Course_id BIGINT,
